3db2b32ab57c0183a3b7ad56668dd4580ffc29b4
isGeekCode <ptuguitar@naver.com>
2023-01-23 21:34:00 +0900

    project INIT

---
+++ SettingApp_Clone/SettingApp_Clone/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingApp_Clone
+//
+//  Created by hyeonseok on 2023/01/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+
+
+}
+
